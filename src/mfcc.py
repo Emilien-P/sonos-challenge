13b10a31d0f0d36595ca1aff47f4571144820cef
alvin-c <alvin.s.cao@gmail.com>
2017-11-28 13:51:20 -0800

    added noise reduction

--- src/mfcc.py
+++ src/mfcc.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-import scipy.io.wavfile as wav
 import numpy as np
 import speechpy, sys
+import scipy.io.wavfile as wav
+from noisereduction import reduce_noise
 
 div = '='*70 + '\n'
 
@@ -13,16 +14,23 @@ def usage():
 
 	print(msg)
 
-def get_mfcc(filename, verbose=False):
+def get_mfcc(filename, noisereduction=True, normalizemean=False, numcoeff=13, verbose=False):
+
 	def print_if(string, verb):
 		if verb:
 			print(string)
 
 	""" Returns the MFCC of a given WAV file as a numpy array """
 
-	print_if('\nMFCC (Mel Frequency Cepstral Coefficients)\n' + div, verbose)
+	# Perform noise reduction before calculating coefficients
+	if noisereduction:
+		print_if('Noise Reduction On', verbose)
+		fs, signal = reduce_noise(filename)
+	else:
+		print_if('Noise Reduction Off', verbose)
+		fs, signal = wav.read(filename)
 
-	fs, signal = wav.read(filename)
+	print_if('\nMFCC (Mel Frequency Cepstral Coefficients)\n' + div, verbose)
 
 	print_if('File sampling frequency: ' + str(fs) + '\n', verbose)
 
@@ -30,7 +38,7 @@ def get_mfcc(filename, verbose=False):
 		                 sampling_frequency = fs, 
 		                 frame_length       = 0.020, 
 		                 frame_stride       = 0.01,
-		                 num_cepstral       = 13,
+		                 num_cepstral       = numcoeff, # Default 13; scale with sample rate
 	                     num_filters        = 40, 
 	                     fft_length         = 512, 
 	                     low_frequency      = 0, 
@@ -41,6 +49,10 @@ def get_mfcc(filename, verbose=False):
 	if verbose:
             print('MFCC - dimension: ', mfcc_feature_cube.shape)
 	print_if(mfcc, verbose)
+
+	# Option to return normalized cepstral mean (for each set of coeffs, subtracts mean from each coeff)
+	if normalizemean:
+		return speechpy.cmvn(mfcc, variance_normalization=False)
 
 	return mfcc
 
@@ -79,5 +91,5 @@ if __name__=="__main__":
 
 	filename = sys.argv[1]
 
-	get_mfcc(filename)
+	get_mfcc(filename, verbose=True)
 	get_mfe(filename)
